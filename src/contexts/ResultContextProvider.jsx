fd793e326cf0cc57ac9d9ffada34bdd4b1b8f369
danildany <76399818+danildany@users.noreply.github.com>
2021-10-21 21:27:37 -0300

    Update ResultContextProvider.jsx

--- src/contexts/ResultContextProvider.jsx
+++ src/contexts/ResultContextProvider.jsx
@@ -5,7 +5,7 @@ const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("Boca juniors");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const getResult = async (type) => {
     setIsLoading(true);
